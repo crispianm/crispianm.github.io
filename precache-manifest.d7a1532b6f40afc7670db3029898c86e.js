668cbd4c3b290a30b6b06185042e77b83102077a
crispianm <crispian.morris@bristol.ac.uk>
2022-06-16 13:15:12 +0100

    Updates

---
+++ precache-manifest.d7a1532b6f40afc7670db3029898c86e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cffd2b69e3e14c055d1b",
+    "url": "/static/css/main.bab58fe4.chunk.css"
+  },
+  {
+    "revision": "cffd2b69e3e14c055d1b",
+    "url": "/static/js/main.cffd2b69.chunk.js"
+  },
+  {
+    "revision": "7774d48e178d68242258",
+    "url": "/static/js/1.7774d48e.chunk.js"
+  },
+  {
+    "revision": "f70ee63ae61155cdf5b5",
+    "url": "/static/js/2.f70ee63a.chunk.js"
+  },
+  {
+    "revision": "4f04b5bcf66441790023",
+    "url": "/static/js/runtime~main.4f04b5bc.js"
+  },
+  {
+    "revision": "83c9dcb81f580e5c89c89320724ef30d",
+    "url": "/index.html"
+  }
+];
